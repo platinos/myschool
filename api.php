51d4cb9ada2c7cb226e7c31f222180809e525def
Priyank Upadhyay <priyank@platinos.in>
2017-09-22 06:58:19 +0530

    error logging

--- api.php
+++ api.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include 'dbconnect.php';
 
 if(isset($_POST["func"]) && !empty($_POST["func"])){
