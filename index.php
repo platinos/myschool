1266caf070962e1623ebb26361f1fe562d13f3ec
Priyank Upadhyay <priyank@platinos.in>
2017-09-22 14:56:28 +0530

    i

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 include 'dbconnect.php';
 $sql = "SELECT * FROM questions";
+echo 'ddd';
     $result = mysqli_query($conn, $sql);
-    $rows = array();
+    //$rows = array();
    while($r = mysql_fetch_assoc($result)) {
      echo $r;
    }
