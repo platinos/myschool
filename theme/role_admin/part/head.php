bb5af43496f5e0b5c0e03da791b4a195a67b91d2
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2017-09-26 04:34:55 +0530

    hi

--- theme/role_admin/part/head.php
+++ theme/role_admin/part/head.php
@@ -26,3 +26,4 @@
 
 <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
 <link href="css/themes/all-themes.css" rel="stylesheet" />
+<?Php error_reporting(E_ALL);?>
