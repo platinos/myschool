9fa7ab8ee77a2a29590740c2d0190ae05de253c9
Priyank Upadhyay <priyank@platinos.in>
2017-09-25 15:42:19 +0530

    bnb

--- recieve.php
+++ recieve.php
@@ -4,6 +4,6 @@
 //   shell_exec( 'cd /usr/share/nginx/html/mypaper && git reset --hard HEAD && git pull' );
 // }
 
-//shell_exec( 'cd /usr/share/nginx/html/mypaper' );
+shell_exec( 'cd /usr/share/nginx/html/myschool && sudo git reset --hard HEAD && sudo git pull' );
 echo "Hi";
 ?>
