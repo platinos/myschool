6c108339f9f8f9241bb0b3e8f7dff1adf87c59f9
Priyank Upadhyay <priyank@platinos.in>
2017-09-25 15:36:51 +0530

    updated

--- recieve.php
+++ recieve.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 // if ( $_POST['payload'] ) {
-shell_exec( 'cd /usr/share/nginx/html/myschool && sudo git reset --hard HEAD && sudo git pull' );
+shell_exec( 'cd /usr/share/nginx/html/myschool' );
 echo "Hi";
 ?>
