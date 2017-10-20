4d1d15c03b84e4c913accb9f59ddf933d7b19d49
Priyank Upadhyay <priyank@platinos.in>
2017-10-20 16:40:15 +0530

    123

--- role_admin/config.php
+++ role_admin/config.php
@@ -9,34 +9,38 @@ function apicall($func_name, $params){
 	$feed = null;
 
 if(isset($func_name) && !empty($func_name)){
+	echo "i m here";
 
 	if(isset($params) && !empty($params)){
-		$func_array = ['func_name' => '$func_name'];
-		$values = array_merge($func_name, $params)
-		$parameters = http_build_query($values);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$api_url);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,$parameters);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$output = curl_exec ($ch);
-		$feed = json_decode($output, true);
+		echo "i got in deep";
+		// $func_array = ['func_name' => '$func_name'];
+		// $values = array_merge($func_name, $params)
+		// $parameters = http_build_query($values);
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL,$api_url);
+		// curl_setopt($ch, CURLOPT_POST, 1);
+		// curl_setopt($ch, CURLOPT_POSTFIELDS,$parameters);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// $output = curl_exec ($ch);
+		// $feed = json_decode($output, true);
 	}
 	else{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$api_url);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,"func=".$func_name);
+		echo "i am on surface";
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL,$api_url);
+		// curl_setopt($ch, CURLOPT_POST, 1);
+		// curl_setopt($ch, CURLOPT_POSTFIELDS,"func=".$func_name);
 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		$output = curl_exec ($ch);
-		$feed = json_decode($output, true);
+		// $output = curl_exec ($ch);
+		// $feed = json_decode($output, true);
 	}
 
 
 }
 else{
+	echo "i was out";
 	$output = [ 'error' => 'Function name not set.'];
 	$feed = json_decode($output, true);
 
