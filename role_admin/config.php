2adb7bbd41ee68ab994899a034f9632a2f4e71d7
Priyank Upadhyay <priyank@platinos.in>
2017-10-20 16:54:18 +0530

    4

--- role_admin/config.php
+++ role_admin/config.php
@@ -13,16 +13,16 @@ if(isset($func_name) && !empty($func_name)){
 
 	if(isset($params) && !empty($params)){
 		echo "i got in deep";
-		// $func_array = ['func_name' => '$func_name'];
-		// $values = array_merge($func_name, $params)
-		// $parameters = http_build_query($values);
-		// $ch = curl_init();
-		// curl_setopt($ch, CURLOPT_URL,$GLOBALS['api_url']);
-		// curl_setopt($ch, CURLOPT_POST, 1);
-		// curl_setopt($ch, CURLOPT_POSTFIELDS,$parameters);
-		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		// $output = curl_exec ($ch);
-		// $feed = json_decode($output, true);
+		$func_array = ['func_name' => '$func_name'];
+		$values = array_merge($func_name, $params)
+		$parameters = http_build_query($values);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$GLOBALS['api_url']);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,$parameters);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$output = curl_exec ($ch);
+		$feed = json_decode($output, true);
 	}
 	else{
 		echo "i am on surface";
