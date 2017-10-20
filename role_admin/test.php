5ed8af5c05d08edcb8d1f72df4c9692e91baf3bd
Priyank Upadhyay <priyank@platinos.in>
2017-10-20 16:53:17 +0530

    5

--- role_admin/test.php
+++ role_admin/test.php
@@ -1,6 +1,15 @@
 <?php
 include 'config.php';
 
+$feed = apicall();
+echo"<hr>";
+
 $feed = apicall("viewquestion");
+echo"<hr>";
+$var =['something'=> 'h'];
+$feed = apicall("viewquestion" ,$var);
+echo"<hr>";
+
+
 var_dump($feed);
 ?>
