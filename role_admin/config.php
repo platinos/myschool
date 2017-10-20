72404f170a77c7904ef5785021e630e232f93ea6
Priyank Upadhyay <priyank@platinos.in>
2017-10-20 16:55:48 +0530

    8

--- role_admin/config.php
+++ role_admin/config.php
@@ -16,13 +16,15 @@ if(isset($func_name) && !empty($func_name)){
 		$func_array = ['func_name' => '$func_name'];
 		$values = array_merge($func_name, $params)
 		$parameters = http_build_query($values);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$GLOBALS['api_url']);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,$parameters);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$output = curl_exec ($ch);
-		$feed = json_decode($output, true);
+		var_dump($parameters);
+
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL,$GLOBALS['api_url']);
+		// curl_setopt($ch, CURLOPT_POST, 1);
+		// curl_setopt($ch, CURLOPT_POSTFIELDS,$parameters);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// $output = curl_exec ($ch);
+		// $feed = json_decode($output, true);
 	}
 	else{
 		echo "i am on surface";
