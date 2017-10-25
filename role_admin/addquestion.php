d6b1edc48cd6bfa421bf61783ae3f6362f5f6439
Priyank Upadhyay <priyank@platinos.in>
2017-10-26 00:24:52 +0530

    4

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -29,7 +29,7 @@
 
 <!-- ***************** FORM **********************-->
 <!-- Middle Column -->
-<div class="col-lg-12-9">
+<div class="col-lg-9">
 <form>
     <textarea id="question">
     Question
@@ -47,107 +47,6 @@
    <h3>QUESTION ATTRIBUTES</h3>
                                         
 
-                      <form class="form-horizontal style-form" name="ques_attr" id="ques_attr" method="post">
-              <BR>
-               
-               <input type="hidden" id="entrytype" value="new">              <input type="hidden" name="subject" id="subject" value="physics">
-              <!-- Text input-->
-               <div class="form-group">
-                <label class="col-md-4 control-label" for="class">Class</label>
-                <div class="col-md-6">
-                <h4>
-                <select id="qclass" name="qclass" size="4" class="form-control" onchange="chap_select()">
-                <option value="9" selected>IX</option>
-                <option value="10">X</option>
-                <option value="11">XI</option>
-                <option value="12">XII</option>
-                </select>
-                </h4>
-                </div>
-                </div>
-            
-
-            <!-- Select Basic -->
-            <div class="form-group">
-              <label class="col-md-3 control-label" for="selectbasic">Type</label>
-              <div class="col-md-9">
-                <select id="type" name="type" size="5" class="form-control" onchange="answerbox();">
-                  <option value="1">MCQ</option>
-                  <option value="2">TRUE/FALSE</option>
-                  <option value="3" selected>SHORT ANSWER</option>
-                  <option value="4">LONG ANSWER</option>
-                  <option value="5">COMPREHENSION</option>
-                </select>
-              </div>
-            </div>
-
-            <!-- Text input-->
-            <div class="form-group">
-              <label class="col-md-4 control-label" for="tag">Tags</label>  
-              <div class="col-md-6">
-              <input id="tag" name="tag" type="text" placeholder="" class="form-control input-md">
-             </div>
-            </div>
-             <!-- Text input-->
-            <div id="chap_sel" class="form-group">
-              <label class="col-md-4 control-label" for="tag">Chapter</label>  
-              <div class="col-md-6">
-                 <select id="chapter" name="chapter" size="1" class="form-control" onchange="topic_select()">                                  <option value="Motion in 1 D">Motion in 1 D</option>
-                              </select>                         
-
-              
-             </div>
-            </div>
-
-
-            <!-- Text input-->
-            <div class="form-group" id="topic_sel">
-              <label class="col-md-4 control-label" for="topic">Topic</label>  
-              <div class="col-md-6">
-<select id="topic" name="topic" size="6" class="form-control">                                  <option value="Test">Test</option>
-                                                                <option value="Test33">Test33</option>
-                                                                <option value="test2">test2</option>
-                                                                <option value="xyz">xyz</option>
-                                                                <option value="test3">test3</option>
-                                                                <option value="biomolecules">biomolecules</option>
-                              </select>                         
-
-
-              
-             </div>
-            </div>
-
-
-
-           
-
-            <!-- Select Basic -->
-            <div class="form-group">
-              <label class="col-md-4 control-label" for="selectbasic">level</label>
-              <div class="col-md-6">
-                <select id="level" name="level" size="4" class="form-control">
-                  <option value="1">Easy</option>
-                  <option value="2">Medium</option>
-                  <option value="3">Tough</option>
-                  <option value="4">HOTS</option>
-                </select>
-              </div>
-            </div>
-
-            <!-- Text input-->
-            <div class="form-group">
-              <label class="col-md-4 control-label" for="textinput">marks alloted</label>  
-              <div class="col-md-6">
-              <input id="marks" name="marks" type="number" value="" class="form-control input-md">
-             
-              </div>
-            </div>
-
-           <input type="submit" id="save" name="save" value="Save Question" class="btn btn-primary btn-lg" onclick='ClickToSave()'/>
-          
-          
-            </form>
-    
 </div>
 
 
