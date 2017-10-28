d72e6217a93ad7cd91d7a173fe72b4d81cfe9d40
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2017-10-28 15:16:24 +0530

    hi

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -31,36 +31,48 @@
 <!-- ***************** FORM **********************-->
 <!-- Middle Column -->
 
-<form>
+<form method="POST" action='' enctype="multipart/form-data">
+  
     <textarea id="question" name="question">
     Question
 </textarea>
 
+<div id=1>
 <textarea id="answer" name="answer">
     Answer
 </textarea>
-</form>
+</div>
 
+<div id=2>
 
+                                <input name="group" type="radio" id="true" checked />
+                                <label for="true" id="true1">True</label>  <BR>
+                                <input name="group" type="radio" id="false" />
+                                <label for="false" id="false1">False</label>
+                                
+</div> 
 
+<div id=3>
+<textarea id="mcq1" name="mcq1">
+    option -1 - please place your correct answer in this field
+</textarea>
 
+<textarea id="mcq2" name="mcq2">
+    option -2
+</textarea>
 
+<textarea id="mcq3" name="mcq3">
+    option -3
+</textarea>
 
+<textarea id="mcq4" name="mcq4">
+    option -4
+</textarea>
+</div>
 
-
-
-<!--  ***************** END FORM ********************** -->
-
-
-
-
-
-
-
-
-                        </div>
-                    </div>
-                </div>
+</div>
+</div>
+</div>
 
 
 
@@ -77,7 +89,7 @@
 <!-- ***************** FORM **********************-->
 
 <!-- Right Column -->
-<form>
+
     
  <label class="form-label" >Class</label>
 <select class="form-control show-tick" name="class" id="class">
@@ -90,14 +102,16 @@
 
                                     <label class="form-label" >Subject</label>
 <select class="form-control show-tick" name="subject" id="subject" onchange="chap_select()">
-                                        <option value="chemistry" selected>Chemistry</option>
+                                        <option value="select" selected>Select</option>
+                                         <option value="chemistry" >Chemistry</option>
                                         <option value="physics">Physics</option>
                                         <option value="mathematics">Mathematics</option>
                                         <option value="english">English</option>
                                     </select>
                                     <br><br>
+
  <label class="form-label">Type</label>
-<select name="type" class="form-control show-tick" id="type" onchange="answerbox();">
+<select name="type" class="form-control" id="type" onchange="just_change(this.value)">                  
                   <option value="1">MCQ</option>
                   <option value="2">TRUE/FALSE</option>
                   <option value="3" selected>SHORT ANSWER</option>
@@ -105,24 +119,23 @@
                   <option value="5">COMPREHENSION</option>
                 </select>
 <br><br>
+
  <label class="form-label">Tag</label>
- <input type="text" class="form-control" name="tag" required>
-       <br>                              
+ <input type="text" class="form-control" name="tag" >
+       <br>                 
+
  <label class="form-label">Chapter</label>
-  <select id="chapters" name="chapters" class="form-control" onchange="topic_select()">                                  
+  <select id="chapters" name="chapters" class="form-control" onchange="topic_select()">
     <option value="Motion in 1 D">Motion in 1 D</option>
                               </select>
-
 <br><br>
+
 <label class="form-label">Topic</label>
-                              <select id="topic" name="topic" size="6" class="form-control">                                  <option value="Test">Test</option>
-                                                                <option value="Test33">Test33</option>
-                                                                <option value="test2">test2</option>
-                                                                <option value="xyz">xyz</option>
-                                                                <option value="test3">test3</option>
-                                                                <option value="biomolecules">biomolecules</option>
+                              <select id="topic" name="topic" size="6" class="form-control"> 
+                               <option value="Test">Loading...</option>              
                               </select>    
 <br><br>
+
 <label class="form-label">Level</label>
                                <select id="level" name="level" size="4" class="form-control">
                   <option value="1">Easy</option>
@@ -130,14 +143,20 @@
                   <option value="3">Tough</option>
                   <option value="4">HOTS</option>
                 </select>   
-
                 <br><br>
+
 <label class="form-label">Marks alloted</label>  
-                              <input id="marks" name="marks" type="number" value="" class="form-control input-md">
+                              <input id="marks" name="marks" required type="number" value="" class="form-control input-md">
                               <br>
+
                               <label class="form-label">Youtube link</label>  
-                              <input id="marks" name="marks" type="text" value="" class="form-control input-md">
+                              <input id="link" name="link" type="text" value="" class="form-control input-md">
                               <br>
+
+                              <label class="form-label">Scanned Copy</label>  
+                              <input type="file" name="upload" id="upload" accept="image/*">
+                              <br>
+                              
 <input type="submit" id="save" name="save" value="Save Question" class="btn btn-primary btn-lg" onclick='ClickToSave()'/>
 </form>
 
@@ -211,12 +230,93 @@
 
     <script type="text/javascript">
 
+$(document).ready(function(){
+  
+        $("#1").show();
+        $("#2").hide();
+        $("#3").hide();   
+});
+
+  
+    
+function just_change() {
+  // body...
+
+  var value = document.getElementById("type").value;
+
+   if(value =="3"||value =="4"||value =="5")
+ {
+        $("#1").show();
+        $("#2").hide();
+        $("#3").hide();  
+         }
+         
+         else if(value=="1")
+ {
+        $("#1").hide();
+        $("#2").hide();
+        $("#3").show();  
+ }
+ else if(value=="2")
+ {
+        $("#1").hide();
+        $("#2").show();
+        $("#3").hide();  
+ }
+}
+
+
+
+
+
+function topic_select(){
+        var form = new FormData();
+form.append("func", "gettopics");
+form.append("ch_id", document.getElementById("chapters").value);
+
+
+var settings = {
+  "async": true,
+  "url": "functions.php",
+  "method": "POST",
+
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+}
+
+$.ajax(settings).done(function (response) {
+  var jsonData1= JSON.parse(response);
+  
+  var dataSize = jsonData1.data.size;
+
+  
+  var str="";
+  for (var i = 0; i < dataSize; i++) {
+    var counter = jsonData1.data[i];
+    str += "<option value='"+counter.id+"'>"+counter.topic+"</option>";
+
+}
+
+
+
+//$('#here').html(str);
+
+var select = $('#topic');
+select.empty().append(str);
+
+
+
+});
+
+     }  
 
      function chap_select(){
         var form = new FormData();
 form.append("func", "getchapters");
-form.append("class", "9");
-form.append("subject", "Chemistry");
+form.append("class", document.getElementById("class").value);
+form.append("subject", document.getElementById("subject").value);
 
 var settings = {
   "async": true,
@@ -235,11 +335,12 @@ $.ajax(settings).done(function (response) {
   var dataSize = jsonData.size;
 
   
-  var str="";
+  var str="<option value=select>Select</option>";
   for (var i = 0; i < jsonData.data.length; i++) {
     var counter = jsonData.data[i];
     str += "<option value='"+counter.id+"'>"+counter.chapter+"</option>";
 }
+
 
  //alert(str);
 
@@ -287,6 +388,67 @@ $(function () {
         toolbar2: 'print preview media | forecolor backcolor emoticons',
         image_advtab: true
     });
+
+    tinymce.init({
+        selector: "textarea#mcq1",
+        theme: "modern",
+        height: 100,
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        image_advtab: true
+    });
+
+    tinymce.init({
+        selector: "textarea#mcq2",
+        theme: "modern",
+        height: 100,
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        image_advtab: true
+    });
+
+    tinymce.init({
+        selector: "textarea#mcq3",
+        theme: "modern",
+        height: 100,
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        image_advtab: true
+    });
+
+    tinymce.init({
+        selector: "textarea#mcq4",
+        theme: "modern",
+        height: 100,
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        image_advtab: true
+    });
+
     tinymce.suffix = ".min";
     tinyMCE.baseURL = 'plugins/tinymce';
 });
