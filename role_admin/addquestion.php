94df8b982c66d0b58876985796c22cd91106a832
Priyank Upadhyay <priyank@platinos.in>
2017-10-29 23:17:19 +0530

    wiris configs

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -476,10 +476,10 @@ tinymce.init({
 	'advlist autolink lists link image charmap print preview hr anchor pagebreak',
 	'searchreplace wordcount visualblocks visualchars code fullscreen',
 	'insertdatetime media nonbreaking save table contextmenu directionality',
-	'emoticons template paste textcolor colorpicker textpattern imagetools'
+	'emoticons template paste textcolor colorpicker textpattern imagetools tiny_mce_wiris'
 	],
 	toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-	toolbar2: 'print preview media | forecolor backcolor emoticons ',
+	toolbar2: 'print preview media | forecolor backcolor emoticons | tiny_mce_wiris_formulaEditor',
 	image_advtab: true
 });
 
