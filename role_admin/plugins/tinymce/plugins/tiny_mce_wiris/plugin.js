6efe7f99b4a51b6134202de45772e96847ca2faf
Priyank Upadhyay <priyank@platinos.in>
2017-10-30 00:05:43 +0530

    m

--- role_admin/plugins/tinymce/plugins/tiny_mce_wiris/plugin.js
+++ role_admin/plugins/tinymce/plugins/tiny_mce_wiris/plugin.js
@@ -11,6 +11,7 @@ if (typeof _wrs_isMoodle24 == 'undefined') {
     _wrs_baseURL = tinymce.baseURL;
     _wrs_baseURL = "/plugins/tinymce";
     _wrs_conf_path = _wrs_baseURL + '/plugins/tiny_mce_wiris/'; // TODO use the same variable name always.
+    _wrs_conf_path = 'plugins/tinymce/plugins/tiny_mce_wiris/'; // TODO use the same variable name always.
 }else{
     var base = tinymce.baseURL;
     var search = 'lib/editor/tinymce';
