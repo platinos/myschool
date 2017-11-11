b786c158722327c81b4480fc3a3834f38aedcf15
Unknown <kartikpawar2012@gmail.com>
2017-11-11 22:28:42 +0530

    jk

--- role_admin/editchapter.php
+++ role_admin/editchapter.php
@@ -44,7 +44,8 @@
                             <form id="form_validation" action="" method="POST">
                                 <div class="form-group form-float">
                                     <div class="form-line" id="name_header">
-                                        <input type="text" id="chap_name" class="form-control" name="chap"  value="<?php var_dump($feed['data']); ?>" required>
+                                  <?php echo '<input type="text" id="chap_name" class="form-control" name="chap"  value="'.$feed["data"][0]["chapter"].'" required>';?>
+                                      
                                         <label class="form-label">Name of The Chapter</label>
                                     </div>
                                 </div>
