c45380ccc65e248dd9f95930001edd7e05316f6a
Unknown <kartikpawar2012@gmail.com>
2017-11-15 20:55:09 +0530

    fdsa

--- role_admin/qpcarttable.php
+++ role_admin/qpcarttable.php
@@ -77,3 +77,18 @@ include_once 'functions.php';
         ?>
     </tbody>
 </table>
+<script src="plugins/jquery/jquery.min.js"></script>
+
+<!-- Jquery DataTable Plugin Js -->
+<script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
+<script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+<!-- Custom Js -->
+<script src="js/pages/tables/jquery-datatable.js"></script>
