a8386dd78fb63f6806909e83f57f631727d96852
Priyank Upadhyay <priyank@platinos.in>
2017-11-15 19:07:19 +0530

    update

--- role_admin/viewquestion.php
+++ role_admin/viewquestion.php
@@ -51,7 +51,7 @@ else
 
                     </div>
                     <div class="body">
-                        <table class="table table-bordered table-striped table-hover dataTable">
+                        <table class="table table-bordered table-striped table-hover dataTable js-exportable">
                             <thead>
                                 <tr>
                                     
