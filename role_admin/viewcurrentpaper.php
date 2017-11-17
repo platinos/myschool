6542c7534561d57ebb34284466f298ab43c539b3
Priyank Upadhyay <priyank@platinos.in>
2017-11-17 22:11:22 +0530

    n b

--- role_admin/viewcurrentpaper.php
+++ role_admin/viewcurrentpaper.php
@@ -45,7 +45,7 @@
 
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="card">
-                <div style='position:relative;'id='questionPaper'>
+                <div id='questionPaper'>
                     <?php include 'printformat.html'; ?>
                 </div>
             </div>
