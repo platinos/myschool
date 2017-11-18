44344ecc89b4c889206aa616636bf02df929726a
Unknown <kartikpawar2012@gmail.com>
2017-11-19 04:26:44 +0530

    fgsd

--- role_admin/templatebody.php
+++ role_admin/templatebody.php
@@ -6,22 +6,21 @@ $questionsFeed=$feed['data'];
 foreach ($questionsFeed as $key => $value) {
 	?>
 	<div id="question">
-		<p><strong id="ques_no">Q.1&nbsp;&nbsp;&nbsp; </strong><span id="ques_txt"><?php echo $questionsFeed[$i]['ques_txt'] ?> </span></p>
-		<p style="text-align: center;">&nbsp;<img src="<?php echo $questionsFeed[$i]['img_src'] ?>" alt="Image here" id="img_src" /></p>
+		<p><strong id="ques_no">Q.<?php $i+1?>&nbsp;&nbsp;&nbsp;</strong><span id="ques_txt"><?php echo $questionsFeed[$i]['ques_txt'] ?> </span></p>
+		<p style="text-align: center;">&nbsp;<img src="<?php echo $questionsFeed[$i]['img_src'] ?>" id="img_src" /></p>
 		<table style="height: 68px; margin-left: auto; margin-right: auto;" border="0px" width="100%">
 			<tbody>
 				<tr style="height: 26px;">
-					<td style="width: 244px; height: 26px;" id="option1"><?php echo $questionsFeed[$i]['option1'] ?></td>
-					<td style="width: 244px; height: 26px;" id="option2"><?php echo $questionsFeed[$i]['option2'] ?></td>
+					<td style="width: 244px; height: 26px;" id="option1">(A)<?php echo $questionsFeed[$i]['option1'] ?></td>
+					<td style="width: 244px; height: 26px;" id="option2">(B)<?php echo $questionsFeed[$i]['option2'] ?></td>
 				</tr>
 				<tr style="height: 26px;">
-					<td style="width: 244px; height: 26px;" id="option3"><?php echo $questionsFeed[$i]['option3'] ?></td>
-					<td style="width: 244px; height: 26px;" id="option4"><?php echo $questionsFeed[$i]['option4'] ?></td>
+					<td style="width: 244px; height: 26px;" id="option3">(C)<?php echo $questionsFeed[$i]['option3'] ?></td>
+					<td style="width: 244px; height: 26px;" id="option4">(D)<?php echo $questionsFeed[$i]['option4'] ?></td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
-	<p>&nbsp;</p>
 	<p>&nbsp;</p>
 <?php
 	$i++;
