8779dce87ad29cd4ba81202e33aef3e4ca9ee9dc
Unknown <kartikpawar2012@gmail.com>
2017-11-19 05:11:14 +0530

    fsdf

--- role_admin/templatebody.php
+++ role_admin/templatebody.php
@@ -5,8 +5,8 @@ $questionsFeed=$feed['data'];
 foreach($questionsFeed as $key=> $value) {
 	?>
 	<div id="question">
-		<p><strong id="ques_no">Q.<?php echo $i+1?>&nbsp;&nbsp;&nbsp;</strong><span id="ques_txt"><?php echo strip_tags($questionsFeed[$i]['ques_txt']) ?> </span></p>
-		<p style="text-align: center;"><img src="<?php echo strip_tags($questionsFeed[$i]['img_src']) ?>" id="img_src" /></p>
+		<strong id="ques_no">Q.<?php echo $i+1?>&nbsp;&nbsp;&nbsp;</strong><span id="ques_txt"><?php echo strip_tags($questionsFeed[$i]['ques_txt']) ?> </span>
+		<img src="<?php echo strip_tags($questionsFeed[$i]['img_src']) ?>" id="img_src" align="middle"/>
 		<table style="height: 68px; margin-left: auto; margin-right: auto;" border="0px" width="100%">
 			<tbody>
 				<tr style="height: 26px;">
