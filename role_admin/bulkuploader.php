92de0fab702793a10fb80b71dbab26b61ad04582
Priyank Upadhyay <priyank@platinos.in>
2017-11-19 14:07:08 +0530

    jbj

--- role_admin/bulkuploader.php
+++ role_admin/bulkuploader.php
@@ -39,7 +39,7 @@
                     </div>
                     <div class="body">
                        <?php 
-                       if(isset($_POST['file'])){
+                       if(isset($_POST)){
                         //if file is uploaded
 
                         $target_dir = "uploads/";
@@ -58,8 +58,8 @@
                         echo "Sorry, your file was not uploaded.";
                         // if everything is ok, try to upload file
                         } else {
-                        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+                        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
                         } else {
                         echo "Sorry, there was an error uploading your file.";
                         }
