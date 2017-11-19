f74e6c2cacae9e48706d69db52bbc8bae48cecdb
Priyank Upadhyay <priyank@platinos.in>
2017-11-19 17:02:49 +0530

    befkoofi

--- role_admin/templatebody.php
+++ role_admin/templatebody.php
@@ -34,7 +34,7 @@ function printSection($sectionFeed){
 		?>
 		<div id="question">
 			<div><strong id="ques_no">Q.<?php echo $i+1?>&nbsp;&nbsp;&nbsp;</strong><span id="ques_txt"><?php echo strip_tags($question['ques_txt']) ?> </span><span id="marks"><?php echo "(Marks: ".strip_tags($question['marks']).")"?></span></div>
-			<div align="center"> <img src="<?php echo strip_tags($question['img_src']) ?>" id="img_src" align="middle"/></div>
+			<div align="center"> <img src="<?php echo strip_tags($question['ques_img']) ?>" id="img_src" align="middle"/></div>
 			<table style="height: 68px; margin-left: auto; margin-right: auto;" border="0px" width="100%">
 				<tbody>
 					<tr style="height: 26px;">
