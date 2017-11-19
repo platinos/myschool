67fd716404c4bb54cbcb9aad0822d3440a009b3a
Priyank Upadhyay <priyank@platinos.in>
2017-11-19 14:41:44 +0530

    zdfxgcvhjk

--- role_admin/bulkuploader.php
+++ role_admin/bulkuploader.php
@@ -17,102 +17,99 @@
 <?php include 'part/nav.php'; ?>
 
 
- 
-
-    <section class="content">
-        <div class="container-fluid">
-
-            <!-- Basic Examples -->
-            
-            <!-- #END# Basic Examples -->
-            <!-- Exportable Table -->
-            <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="card">
-                        <div class="header">
-
-                           
-                         <h2>
-                            Bulk Upload Questions.
-                        </h2>
-
-                    </div>
-                    <div class="body">
-                       <?php 
-                       if(isset($_POST["submitVal"])){
-                        //if file is uploaded
-
-                        $target_dir = "uploads/";
-                        $target_file = $target_dir . basename($_FILES["file"]["name"]);
-                        $uploadOk = 1;
-                        $csvFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-                        // Allow certain file formats
-                       
-                        // $csv_mimetypes = array(
-                        //     'text/csv',
-                        //     'text/plain',
-                        //     'application/csv',
-                        //     'text/comma-separated-values',
-                        //     'application/excel',
-                        //     'application/vnd.ms-excel',
-                        //     'application/vnd.msexcel',
-                        //     'text/anytext',
-                        //     'application/octet-stream',
-                        //     'application/txt',
-                        // );
-
-                        // if (!in_array($_FILES['file']['type'], $csv_mimetypes)) {
-                        //     echo "Sorry, only csv files are allowed.";
-                        //     $uploadOk = 0;
-                        // }
-                        // Check if $uploadOk is set to 0 by an error
-                        if ($uploadOk == 0) {
-                        echo "Sorry, your file was not uploaded.";
-                        // if everything is ok, try to upload file
-                        } else {
-                        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-                        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
-                        } else {
-                        echo "Sorry, there was an error uploading your file.";
-                        }
-                        }
-
-                        
-
-                       }
-                       else{
-                        ?>
 
 
-                        <!-- form here --> 
-                       <form action="bulkuploader.php" id="frmFileUpload" class="dropzone" method="post" enctype="multipart/form-data">
-                                <div class="dz-message">
-                                    <div class="drag-icon-cph">
-                                        <i class="material-icons">touch_app</i>
-                                    </div>
-                                    <h3>Drop files here or click to upload CSV file.</h3>
-                                                                    </div>
-                                <div class="fallback">
-                                    <input name="file" type="file" multiple accept=".csv"/>
-                                </div>
-                                <input type="hidden" name="submitVal" value="2">
-                                
-                            </form>
-                            <br>
-                                <br>
-                                <button type="button" class="btn btn-info waves-effect" onclick="document.getElementById('frmFileUpload').submit();">Submit</button>
+<section class="content">
+    <div class="container-fluid">
+
+        <!-- Basic Examples -->
+
+        <!-- #END# Basic Examples -->
+        <!-- Exportable Table -->
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
 
 
-                        <?php
-                       }
-                       ?>
+                       <h2>
+                        Bulk Upload Questions.
+                    </h2>
 
                 </div>
+                <div class="body">
+                 <?php 
+                 if(isset($_POST["submitVal"])){
+                        //if file is uploaded
+
+                    $target_dir = "uploads/";
+                    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+                    $uploadOk = 1;
+                    $csvFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+                        //Allow certain file formats
+
+                        $csv_mimetypes = array(
+                            'text/csv',
+                            'text/plain',
+                            'application/csv',
+                            'text/comma-separated-values',
+                            'application/excel',
+                            'application/vnd.ms-excel',
+                            'application/vnd.msexcel',
+                            'text/anytext',
+                            'application/octet-stream',
+                            'application/txt',
+                        );
+
+                        if (!in_array($_FILES['file']['type'], $csv_mimetypes)) {
+                            echo "Sorry, only csv files are allowed.";
+                            $uploadOk = 0;
+                        }
+                        // Check if $uploadOk is set to 0 by an error
+                    if ($uploadOk == 0) {
+                        echo "Sorry, your file was not uploaded.";
+                        // if everything is ok, try to upload file
+                    } else {
+                        move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+                        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+                    }
+
+
+
+                }
+                else{
+                    ?>
+
+
+                    <!-- form here --> 
+                    <form action="bulkuploader.php" id="frmFileUpload" class="dropzone" method="post" enctype="multipart/form-data">
+                        <div class="dz-message">
+                            <div class="drag-icon-cph">
+                                <i class="material-icons">touch_app</i>
+                            </div>
+                            <h3>Drop files here or click to upload CSV file.</h3>
+                        </div>
+                        <div class="fallback">
+                            <input name="file" type="file" multiple accept=".csv"/>
+                        </div>
+                        <input type="hidden" name="submitVal" value="2">
+
+                    </form>
+                    <br>
+                    <br>
+                    <button type="button" class="btn btn-info waves-effect" onclick="document.getElementById('frmFileUpload').submit();">Submit</button>
+
+
+                    <?php
+                }
+                ?>
+
             </div>
         </div>
     </div>
-    <!-- #END# Exportable Table -->
+</div>
+<!-- #END# Exportable Table -->
 </div>
 </section>
 
@@ -143,8 +140,8 @@
 <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
 <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
 <script src="plugins/bootstrap-notify/bootstrap-notify.js"></script>
- <!-- Dropzone Plugin Js -->
-    <script src="plugins/dropzone/dropzone.js"></script>
+<!-- Dropzone Plugin Js -->
+<script src="plugins/dropzone/dropzone.js"></script>
 
 
 <!-- Custom Js -->
