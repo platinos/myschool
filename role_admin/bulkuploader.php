e0648436a7bba5df5b3260deca8649b429eb8d7b
Priyank Upadhyay <priyank@platinos.in>
2017-11-19 14:09:20 +0530

    lkbjhbn

--- role_admin/bulkuploader.php
+++ role_admin/bulkuploader.php
@@ -83,11 +83,13 @@
                                 <div class="fallback">
                                     <input name="file" type="file" multiple />
                                 </div>
+
+                                <button type="submit" class="btn btn-info waves-effect" >Submit</button>
                                 
                             </form>
                             <br>
                                 <br>
-                                <button type="button" class="btn btn-info waves-effect" onclick="document.getElementById('frmFileUpload').submit();">Submit</button>
+                                
 
 
                         <?php
