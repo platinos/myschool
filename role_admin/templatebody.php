3bda9ae5211ba952a055f2f65482d9314bf6d649
Unknown <kartikpawar2012@gmail.com>
2017-11-19 15:12:52 +0530

    dfgd

--- role_admin/templatebody.php
+++ role_admin/templatebody.php
@@ -1,29 +1,25 @@
 <!-- removing p tags by using strip_tags to ensure no new line is created by default-->
-//inside template body
 <?php 
 
 $i=0;
 $questionsFeed=$feed['data'];
-$sectionFeed=array();
+$sectionsFeed=array();
 foreach ($questionsFeed as $key => $value) {
-	if($sectionFeed[$questionsFeed[$i]['section']]==null){
-		$sectionFeed[$questionsFeed[$i]['section']]=array();
+	if($sectionsFeed[$questionsFeed[$i]['section']]==null){
+		echo 'inserting array for '.$questionsFeed[$i]['section'];
+		$sectionsFeed[$questionsFeed[$i]['section']]=array();
 	}
-	//echo "\nsectionFeed section:".$questionsFeed[$i]['section']." question: ".$questionsFeed[$i]['ques_txt'];
-	array_push($sectionFeed[$questionsFeed[$i]['section']],$questionsFeed[$i]);
-	//echo "\nin array ".$sectionFeed[$questionsFeed[$i]['section']][0]['ques_txt'];
+	array_push($sectionsFeed[$questionsFeed[$i]['section']],$questionsFeed[$i]);
 	$i++;
 }
 
 $i=0;
-echo "\n no of sections: ".count($sectionFeed);
-foreach($sectionFeed as $key=> $value) {
-	echo "\nquestion count in section: ".count($sectionFeed);
-	//	ksort($sectionFeed);
+foreach($sectionsFeed as $key=> $value) {
+	echo "\nquestion count in section: ".count($sectionsFeed[$key]);
 	?>
 	<section>
-		<p>Section: <?php echo 'key is'.$key.'we are in section '.$sectionFeed[$i][$key]['section']?></p><br>
-		<?php printSection($sectionFeed); ?>
+		<p>Section: <?php ?></p><br>
+		<?php printSection($sectionsFeed[$key]); ?>
 	</section>
 
 <?php
@@ -34,21 +30,20 @@ $i++;
 <?php 
 function printSection($sectionFeed){
 	$i=0;
-	echo "inside printsection";
 	foreach($sectionFeed as $key=> $value) {
 		?>
 		<div id="question">
-			<strong id="ques_no">Q.<?php echo $i+1?>&nbsp;&nbsp;&nbsp;</strong><span id="ques_txt"><?php echo strip_tags($sectionFeed[$i]['ques_txt']) ?> </span>
-			<p align="center"> <img src="<?php echo strip_tags($sectionFeed[$i]['img_src']) ?>" id="img_src" align="middle"/></p>
+			<strong id="ques_no">Q.<?php echo $i+1?>&nbsp;&nbsp;&nbsp;</strong><span id="ques_txt"><?php echo strip_tags($sectionFeed[$key]['ques_txt']) ?> </span>
+			<p align="center"> <img src="<?php echo strip_tags($sectionFeed[$key]['img_src']) ?>" id="img_src" align="middle"/></p>
 			<table style="height: 68px; margin-left: auto; margin-right: auto;" border="0px" width="100%">
 				<tbody>
 					<tr style="height: 26px;">
-						<td style="width: 50%; height: 26px;" id="option1">(A)<?php echo strip_tags($sectionFeed[$i]['option1']) ?></td>
-						<td style="width: 50%; 244px; height: 26px;" id="option2">(B)<?php echo strip_tags($sectionFeed[$i]['option2']) ?></td>
+						<td style="width: 50%; height: 26px;" id="option1">(A)<?php echo strip_tags($sectionFeed[$key]['option1']) ?></td>
+						<td style="width: 50%; 244px; height: 26px;" id="option2">(B)<?php echo strip_tags($sectionFeed[$key]['option2']) ?></td>
 					</tr>
 					<tr style="height: 26px;">
-						<td style="width: 50%; height: 26px;" id="option3">(C)<?php strip_tags($sectionFeed[$i]['option3']) ?></td>
-						<td style="width: 50%; height: 26px;" id="option4">(D)<?php strip_tags($sectionFeed[$i]['option4']) ?></td>
+						<td style="width: 50%; height: 26px;" id="option3">(C)<?php strip_tags($sectionFeed[$key]['option3']) ?></td>
+						<td style="width: 50%; height: 26px;" id="option4">(D)<?php strip_tags($sectionFeed[$key]['option4']) ?></td>
 					</tr>
 				</tbody>
 			</table>
