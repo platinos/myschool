fae272785d5a570ceed5ae8fbf942fbd47bb9903
Unknown <kartikpawar2012@gmail.com>
2017-11-19 16:21:41 +0530

    zv

--- role_admin/templatehead.php
+++ role_admin/templatehead.php
@@ -5,12 +5,12 @@
 <table style="height: 9px;" border="0px" width="80%" align="center">
 	<tbody>
 		<tr style="height: 15px;">
-			<td style="width: 50%; height: 15px;" id="date"><?php echo $_GET['date'] ?></td>
+			<td style="width: 50%; height: 15px;" id="date"><?php echo "Date: ".$_GET['date'] ?></td>
 			<td style="width: 50%; text-align: right; height: 15px;" id="marks"><?php echo "F.M.: ".$_GET['fmarks'] ?> </td>
 		</tr>
 		<tr style="height: 15px;">
 			<td style="width: 50%; height: 15px;" id="subject"><?php echo "Subject: ".$_GET['subject'] ?></td>
-			<td style="width: 50%; text-align: right; height: 15px;" id="time"><?php echo "Time: ".$_GET['time']."mins"
+			<td style="width: 50%; text-align: right; height: 15px;" id="time"><?php echo "Time: ".$_GET['time']." mins"
 			 ?></td>
 		</tr>
 	</tbody>
