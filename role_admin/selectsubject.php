0b1b51357e0a39586518d4b89273d2d0b92f8700
Unknown <kartikpawar2012@gmail.com>
2017-11-26 01:12:57 +0530

    kjhdf

--- role_admin/selectsubject.php
+++ role_admin/selectsubject.php
@@ -1,11 +1,17 @@
+<?php 
+$feed = apicall("viewquestion"); ?>
 <div>
-   <select class="form-control" >
-      <option value="">-- Please select --</option>
-      <?php
-         $i=0;
-         foreach ($subjects as $subject) {
-      ?>
-      <option value="<?php $subject ?>"><?php $subject ?></option>
-       <?php } ?>
-   </select>
-</div>
+ <select class="form-control show-tick" name="subj" id="subj">
+   <option value="select" selected>Select</option>
+   <?php
+
+   $size = $feed['data']['size']; 
+   for($i=0; $i<$size; $i++)
+     {?>
+
+        <option value=<?php echo $feed['data'][$i]['name'] ?> ><?php echo $feed['data'][$i]['name'] ?></option>
+        <?php  }
+        ?>
+
+     </select>
+  </div>
