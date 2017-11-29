6ff3e837c94d2f032600d38cd0023bd2781adb8a
Priyank Upadhyay <priyank@platinos.in>
2017-11-30 01:20:52 +0530

    hkjgjfh

--- role_admin/part/footer.php
+++ role_admin/part/footer.php
@@ -25,22 +25,7 @@
 <!-- Custom Js -->
 <script src="js/admin.js"></script>
 <script src="js/pages/index.js"></script>
-<script src="js/pages/jquery-1.12.4.js"></script>
-<script src="js/pages/jquery.dataTables.min.js"></script>
-
-<!-- Demo Js -->
-
- <script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
-    <script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
 
 
-    <script src="js/pages/tables/jquery-datatable.js"></script>
-    <script src="js/demo.js"></script>
+
 </body>
