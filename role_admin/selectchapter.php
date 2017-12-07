95fc0d4a7679f4bcd0aadd3d6932aaf46d8d9900
Unknown <kartikpawar2012@gmail.com>
2017-12-08 00:44:35 +0530

    afg

---
+++ role_admin/selectchapter.php
@@ -0,0 +1,19 @@
+<?php 
+$subj='chemistry';
+$class=11;
+$value = array("subject"=>$subj,"class"=>$class);
+$feedchapters = apicall("getchapters",$value); ?>
+
+<select onchange="filter();" class="form-control" name="subj" id="subj">
+<option value="select" selected>Select chapters</option>
+   <?php
+
+   $size = $feedchapters['data']['size']; 
+   for($i=0; $i<$size; $i++)
+    {?>
+
+       <option value=<?php echo $feedsubjects['data'][$i]['chapter'] ?> ><?php echo $feedsubjects['data'][$i]['chapter'] ?></option>
+       <?php  }
+       ?>
+
+    </select>
