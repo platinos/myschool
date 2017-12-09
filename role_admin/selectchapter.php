3cb9369e5415ba04a3ef732e30467aad1181a5a8
Unknown <kartikpawar2012@gmail.com>
2017-12-10 00:38:38 +0530

    hhvhv

--- role_admin/selectchapter.php
+++ role_admin/selectchapter.php
@@ -1,3 +1,20 @@
-<select onchange="filter();" class="form-control" name="chapters" id="chapters">
-	<option value="select" selected>Loading</option>
-</select>
+<?php 
+$subj="chemistry";
+$class="9";
+$value = array("subject"=>$subj,"class"=>$class);
+
+$feedchapters = apicall("getchapters",$value); ?>
+
+<select onchange="filter();" class="form-control" name="chapter" id="chapter">
+<option value="select" selected>Select chapters</option>
+   <?php
+
+   $size = $feedchapters['size']; 
+   for($i=0; $i<$size; $i++)
+    {
+    	?>
+       <option value=<?php echo $feedchapters['data'][$i]['chapter'] ?> ><?php echo $feedchapters['data'][$i]['chapter'] ?></option>
+       <?php  }
+       ?>
+
+    </select>
