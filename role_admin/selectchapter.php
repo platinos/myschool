9faca066808df629d5c54d4308d781f5792b1759
Unknown <kartikpawar2012@gmail.com>
2017-12-10 00:14:13 +0530

    Auto stash before merge of "master" and "origin/master"

--- role_admin/selectchapter.php
+++ role_admin/selectchapter.php
@@ -1,20 +1,3 @@
-<?php 
-$subj="chemistry";
-$class="9";
-$value = array("subject"=>$subj,"class"=>$class);
-
-$feedchapters = apicall("getchapters",$value); ?>
-
 <select onchange="filter();" class="form-control" name="chapter" id="chapter">
-<option value="select" selected>Select chapters</option>
-   <?php
-
-   $size = $feedchapters['size']; 
-   for($i=0; $i<$size; $i++)
-    {
-    	?>
-       <option value=<?php echo $feedchapters['data'][$i]['chapter'] ?> ><?php echo $feedchapters['data'][$i]['chapter'] ?></option>
-       <?php  }
-       ?>
-
-    </select>
+	<option value="select" selected>Loading</option>
+</select>
