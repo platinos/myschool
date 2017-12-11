2e75f2f9300bf8f97dff4404cf360b648eb700cc
Priyank Upadhyay <priyank@platinos.in>
2017-12-12 02:35:36 +0530

    qwerty

--- role_admin/viewcurrentpaper.php
+++ role_admin/viewcurrentpaper.php
@@ -122,12 +122,11 @@ tinymce.init({
     'advlist autolink lists link image charmap print preview hr anchor pagebreak',
     'searchreplace wordcount visualblocks visualchars code fullscreen',
     'insertdatetime media nonbreaking save table contextmenu directionality',
-    'emoticons template paste textcolor colorpicker textpattern imagetools'
+    'emoticons template paste textcolor colorpicker textpattern imagetools tiny_mce_wiris'
     ],
     toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    toolbar2: 'print preview media | forecolor backcolor emoticons',
-    image_advtab: true,
-    
+    toolbar2: 'print preview media | forecolor backcolor emoticons | iny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
+    image_advtab: true
 });
 
 tinymce.suffix = ".min";
