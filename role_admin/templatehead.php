f367c417f00bf5bdfa9338ce46ed32bddd7148cb
Unknown <kartikpawar2012@gmail.com>
2017-12-17 01:44:37 +0530

    ytrewq

--- role_admin/templatehead.php
+++ role_admin/templatehead.php
@@ -5,12 +5,12 @@
 <table class="head_table" border="0px">
 	<tbody>
 		<tr >
-			<td class="head_props" id="date"><h3><?php echo "Date: ".$_GET['date'] ?></h3></td>
-			<td class="head_props right" id="t_marks"><h3><?php echo "F.M.: ".$_GET['fmarks'] ?></h3> </td>
+			<td class="head_props" id="date"><h4><?php echo "Date: ".$_GET['date'] ?></h4></td>
+			<td class="head_props right" id="t_marks"><h4><?php echo "F.M.: ".$_GET['fmarks'] ?></h4> </td>
 		</tr>
 		<tr>
-			<td class="head_props" id="subject"><h3><?php echo "Subject: ".$_GET['subject'] ?></h3></td>
-			<td class="head_props right" id="time"><h3><?php echo "Time: ".$_GET['time']." mins" ?></h3></td>
+			<td class="head_props" id="subject"><h4><?php echo "Subject: ".$_GET['subject'] ?></h4></td>
+			<td class="head_props right" id="time"><h4><?php echo "Time: ".$_GET['time']." mins" ?></h4></td>
 			</tr>
 		</tbody>
 	</table>
