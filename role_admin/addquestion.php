61eb7d77970ab7d2604b5d09aa9d41196e2c4597
Priyank Upadhyay <priyank@platinos.in>
2017-12-19 02:04:05 +0530

    qwerty

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -489,10 +489,34 @@ tinymce.init({
 	image_advtab: true,
 	images_upload_url: 'postuploadhandler.php',
 	images_upload_handler: function (blobInfo, success, failure) {
-    setTimeout(function() {
-      // no matter what you upload, we will turn it into TinyMCE logo :)
-      success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
-    }, 2000);
+    var xhr, formData;
+
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.open('POST', 'postAccepter.php');
+
+    xhr.onload = function() {
+      var json;
+
+      if (xhr.status != 200) {
+        failure('HTTP Error: ' + xhr.status);
+        return;
+      }
+
+      json = JSON.parse(xhr.responseText);
+
+      if (!json || typeof json.location != 'string') {
+        failure('Invalid JSON: ' + xhr.responseText);
+        return;
+      }
+
+      success(json.location);
+    };
+
+    formData = new FormData();
+    formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+    xhr.send(formData);
   }
 	
 });
