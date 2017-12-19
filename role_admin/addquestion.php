a179c2265aef7f68c7d6304b75f63dae25e15af3
Priyank Upadhyay <priyank@platinos.in>
2017-12-20 00:33:06 +0530

    qwerty

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -490,36 +490,36 @@ tinymce.init({
 	images_upload_url: 'postAcceptor.php',
   
   // we override default upload handler to simulate successful upload
-  images_upload_handler: function (blobInfo, success, failure) {
-  	var xhr, formData;
+  // images_upload_handler: function (blobInfo, success, failure) {
+  // 	var xhr, formData;
 
-    xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open('POST', 'postAcceptor.php');
+  //   xhr = new XMLHttpRequest();
+  //   xhr.withCredentials = false;
+  //   xhr.open('POST', 'postAcceptor.php');
 
-    xhr.onload = function() {
-      var json;
+  //   xhr.onload = function() {
+  //     var json;
 
-      if (xhr.status != 200) {
-        failure('HTTP Error: ' + xhr.status);
-        return;
-      }
+  //     if (xhr.status != 200) {
+  //       failure('HTTP Error: ' + xhr.status);
+  //       return;
+  //     }
 
-      json = JSON.parse(xhr.responseText);
+  //     json = JSON.parse(xhr.responseText);
 
-      if (!json || typeof json.location != 'string') {
-        failure('Invalid JSON: ' + xhr.responseText);
-        return;
-      }
+  //     if (!json || typeof json.location != 'string') {
+  //       failure('Invalid JSON: ' + xhr.responseText);
+  //       return;
+  //     }
 
-      success(json.location);
-    };
+  //     success(json.location);
+  //   };
 
-    formData = new FormData();
-    formData.append('file', blobInfo.blob(), blobInfo.filename());
+  //   formData = new FormData();
+  //   formData.append('file', blobInfo.blob(), blobInfo.filename());
 
-    xhr.send(formData);
-  },
+  //   xhr.send(formData);
+  // },
 
 });
 
