0a567ab036b02643b2226380a9ba93187b0c3d86
Priyank Upadhyay <priyank@platinos.in>
2017-12-20 01:27:42 +0530

    wertyu

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -485,7 +485,7 @@ tinymce.init({
 	'emoticons template paste textcolor colorpicker textpattern imagetools tiny_mce_wiris'
 	],
 	toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-	toolbar2: 'print preview media | forecolor backcolor emoticons |  fontsizeselect | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
+	toolbar2: 'print preview media | forecolor backcolor emoticons |  fontsizeselect | superscript subscript | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
 	image_advtab: true,
 	images_upload_url: 'postAcceptor.php'
 });
