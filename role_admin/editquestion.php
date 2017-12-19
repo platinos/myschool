ee59a6268f1ce6ec4e0f88e7d20e323f78368b05
Unknown <kartikpawar2012@gmail.com>
2017-12-20 01:56:27 +0530

    ytrewq

--- role_admin/editquestion.php
+++ role_admin/editquestion.php
@@ -200,8 +200,8 @@
 									<br>
 
 									<label class="form-label">Image</label> <br>
-									<p><?php echo $feed['data'][$i]['ques_img'] ?></p>
-									<img src="<?php echo $feed['data'][$i]['ques_img'] ?>" height="100px" width="100px"><br>
+									<p><?php echo $feedquestion['data'][$i]['ques_img'] ?></p>
+									<img src="<?php echo $feedquestion['data'][$i]['ques_img'] ?>" height="100px" width="100px"><br>
 
 									<label class="form-label">File upload</label> 
 									<input type="file" name="file_upload" id="upload" required accept="image/*">
