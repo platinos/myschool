a14c2f3f90f267a21593b0e6c4391de7a0541aad
Unknown <kartikpawar2012@gmail.com>
2017-12-20 00:20:16 +0530

    Auto stash before merge of "master" and "origin/master"

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -487,6 +487,15 @@ tinymce.init({
 	toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
 	toolbar2: 'print preview media | forecolor backcolor emoticons |  fontsizeselect | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
 	image_advtab: true,
+	images_upload_url: 'postAcceptor.php',
+  
+  // we override default upload handler to simulate successful upload
+  images_upload_handler: function (blobInfo, success, failure) {
+    setTimeout(function() {
+      // no matter what you upload, we will turn it into TinyMCE logo :)
+      success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
+    }, 2000);
+  },
 
 });
 
