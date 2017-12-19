5f44f154d694cc4715950748c524ad2ec53b5380
Unknown <kartikpawar2012@gmail.com>
2017-12-19 23:28:40 +0530

    ytrewq

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -482,12 +482,11 @@ tinymce.init({
 	'advlist autolink lists link charmap print preview hr anchor pagebreak',
 	'searchreplace wordcount visualblocks visualchars code fullscreen',
 	'insertdatetime media nonbreaking save table contextmenu directionality',
-	'emoticons template paste textcolor colorpicker textpattern tiny_mce_wiris',
-	'image code'
+	'emoticons template paste textcolor colorpicker textpattern tiny_mce_wiris'
 	],
 	images_upload_url: 'postAcceptor.php',
-	toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | undo redo | image code',
-	toolbar2: 'print preview media | forecolor backcolor emoticons |  fontsizeselect | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
+	toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
+	toolbar2: 'print prepend(nodes...: Node|DOMString)eview media | forecolor backcolor emoticons |  fontsizeselect | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
 	image_advtab: true,
 	// images_upload_url: 'postuploadhandler.php',
 	// images_upload_handler: function (blobInfo, success, failure) {
