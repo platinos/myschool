e85625088a53b7b467db5eeca10b4ead7717d272
Unknown <kartikpawar2012@gmail.com>
2017-12-24 03:05:04 +0530

    ytrewq

--- role_admin/addquestion.php
+++ role_admin/addquestion.php
@@ -315,6 +315,7 @@
 <!-- Custom Js -->
 <script src="js/admin.js"></script>
 <script src="js/pages/tables/jquery-datatable.js"></script>
+<script src="js/question-paper-creator.js"></script>
 <!--  <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script> -->
 
 <!-- Demo Js -->
