aeed0de02a77ca12e07189f993091c9307e0b5cd
Unknown <kartikpawar2012@gmail.com>
2018-01-09 01:34:52 +0530

    ytrewq

--- role_admin/js/inlineeditquestion.js
+++ role_admin/js/inlineeditquestion.js
@@ -7,36 +7,36 @@ function editQuestion(rowid){
 function saveQuestion(questionid, rowid){
     var link=$('#youtube'+rowid+' textarea').val();
     
+    var topic=$('#topic'+rowid).val();
+    var cls=$('#class'+rowid).val();
+    var type=$('#type'+rowid).val();
+    var subject=$('#subject'+rowid).val();
+    var chapter=$('#chapter'+rowid).val();
+    var tag=$('#tag'+rowid).val();
+    var level=$('#level'+rowid).val();
+    var marks=$('#marks'+rowid).val();
     var question=$('#question'+rowid).val();
     var answer=$('#answer'+rowid).val();
-    var topic=$('#topic'+rowid).val();
     var mcq1=$('#mcq1'+rowid).val();
     var mcq2=$('#mcq2'+rowid).val();
     var mcq3=$('#mcq3'+rowid).val();
     var mcq4=$('#mcq4'+rowid).val();
-    var cls=$('#class'+rowid).val();
-    var subject=$('#subject'+rowid).val();
-    var type=$('#type'+rowid).val();
-    var tag=$('#tag'+rowid).val();
-    var chapter=$('#chapter'+rowid).val();
-    var level=$('#level'+rowid).val();
-    var marks=$('#marks'+rowid).val();
     
     var form = new FormData();
     form.append("func", "editquestion");
-    form.append("id", questionid); 
-    form.append("topic", topic); 
+    form.append("id", questionid);
     form.append("question", question); 
-    form.append("answer", answer);
+    form.append("answer", answer); 
     form.append("mcq1", mcq1);
     form.append("mcq2", mcq2);
     form.append("mcq3", mcq3);
-    form.append("mcq4", mcq4);
+    form.append("mcq4", mcq4); 
     form.append("class", cls);
     form.append("subject", subject);
     form.append("type", type);
     form.append("tag", tag);
     form.append("chapter", chapter);
+    form.append("topic", topic);
     form.append("level", level);
     form.append("marks", marks);
     form.append("link", link);
@@ -54,6 +54,7 @@ function saveQuestion(questionid, rowid){
     }
 
     $.ajax(settings).done(function (response) {
+        alert(response);
         $('#youtube'+rowid).html(link);
     });
 }
