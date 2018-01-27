f365b0e506d5479fa29c1eb554fc740a3723b01f
Priyank Upadhyay <priyank@platinos.in>
2018-01-27 18:09:11 +0530

    qwerty

--- role_admin/mstestapi.php
+++ role_admin/mstestapi.php
@@ -5,26 +5,27 @@ include 'config.php';
 header("Access-Control-Allow-Origin: *");
 
 $json = file_get_contents('php://input');
-$obj = json_decode($json, true);
+$obj = json_decode($json);
 var_dump($obj);
-$func = $obj['func'];
-echo $func;
-$params = getparams();
-$feed = apicall($func,$params);
-var_dump($params);
-var_dump($feed);
-echo json_encode($feed);
+//$func = $obj['func'];
+//$func = $obj
+//echo $func;
+//$params = getparams();
+//$feed = apicall($func,$params);
+//var_dump($params);
+//var_dump($feed);
+//echo json_encode($feed);
 
-function getparams()
-{
-	$params = array();
+// function getparams()
+// {
+// 	$params = array();
 
-	foreach ($obj as $key => $value) { 
+// 	foreach ($obj as $key => $value) { 
 
-		if($key != 'func')	$params[$key] = $value;
+// 		if($key != 'func')	$params[$key] = $value;
 		
 
-	}
-	return $params;
-}
+// 	}
+// 	return $params;
+// }
 ?>
