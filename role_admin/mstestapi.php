5f6f9ccf9f6a9ed837e64e6fd24a28f024630b03
Priyank Upadhyay <priyank@platinos.in>
2018-01-27 18:15:14 +0530

    werty

--- role_admin/mstestapi.php
+++ role_admin/mstestapi.php
@@ -6,34 +6,34 @@ header("Access-Control-Allow-Origin: *");
 
 
 $inputJSON = file_get_contents('php://input');
-$input= json_decode( $inputJSON, TRUE ); //convert JSON into array
+$obj= json_decode( $inputJSON, TRUE ); //convert JSON into array
 
-print_r(json_encode($input));
+//print_r(json_encode($obj));
 
 
 
 // $json = file_get_contents('php://input');
 // $obj = json_decode($json);
 // var_dump($obj);
-//$func = $obj['func'];
+$func = $obj['func'];
 //$func = $obj
 //echo $func;
-//$params = getparams();
-//$feed = apicall($func,$params);
+$params = getparams($obj);
+$feed = apicall($func,$params);
 //var_dump($params);
 //var_dump($feed);
-//echo json_encode($feed);
+echo json_encode($feed);
 
-// function getparams()
-// {
-// 	$params = array();
+function getparams($obj)
+{
+	$params = array();
 
-// 	foreach ($obj as $key => $value) { 
+	foreach ($obj as $key => $value) { 
 
-// 		if($key != 'func')	$params[$key] = $value;
+		if($key != 'func')	$params[$key] = $value;
 		
 
-// 	}
-// 	return $params;
-// }
+	}
+	return $params;
+}
 ?>
