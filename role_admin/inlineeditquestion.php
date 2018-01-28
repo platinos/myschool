fa3cf3ae640fc71fef585ab71a355f08c9b8bdd4
Unknown <kartikpawar2012@gmail.com>
2018-01-29 02:45:35 +0530

    ytrewq

--- role_admin/inlineeditquestion.php
+++ role_admin/inlineeditquestion.php
@@ -127,12 +127,12 @@ else
                                         <td id="<?php echo 'question'.$i ?>"><?php echo $feed['data'][$i]['ques_txt'] ?></td>
                                         
                                         <td id="<?php echo 'class'.$i ?>"><?php echo $feed['data'][$i]['class'] ?></td>
-                                        <td><?php echo mapQuesType($feed['data'][$i]['type']).$i ?></td>
+                                        <td id="<?php echo 'type0'.$i ?>"><?php echo mapQuesType($feed['data'][$i]['type']) ?></td>
                                         <td id="<?php echo 'subject'.$i ?>"><?php echo $feed['data'][$i]['subject'] ?></td>
                                         <td id="<?php echo 'chapter'.$i ?>"><?php echo $feed['data'][$i]['chapter'] ?></td>
                                         <td id="<?php echo 'topic'.$i ?>"><?php echo $feed['data'][$i]['topic'] ?></td>
                                         <td id="<?php echo 'answer'.$i ?>"><?php echo htmlspecialchars_decode($feed['data'][$i]['answer']) ?></td>
-                                        <td><?php echo mapDifficulty($feed['data'][$i]['level']).$i; ?></td>
+                                        <td id="<?php echo 'level0'.$i ?>"><?php echo mapDifficulty($feed['data'][$i]['level']) ?></td>
                                         <td id="<?php echo 'marks'.$i ?>"><?php echo $feed['data'][$i]['marks'] ?></td>
                                         <!--td>
                                            <a target="_blank" href = "< ?php echo $feed['data'][$i]['ques_img'] ?>" class = "thumbnail">
