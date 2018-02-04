5e830f1ae0bcd813012872e75f08f0286b36e5a6
Unknown <kartikpawar2012@gmail.com>
2018-02-04 12:50:14 +0530

    ytrewq

--- role_admin/inlineeditquestion.php
+++ role_admin/inlineeditquestion.php
@@ -127,12 +127,12 @@ else
                                         <td id="<?php echo 'question'.$i ?>"><?php echo $feed['data'][$i]['ques_txt'] ?></td>
                                         
                                         <td id="<?php echo 'class'.$i ?>"><?php echo $feed['data'][$i]['class'] ?></td>
-                                        <td id="<?php echo 'type0'.$i ?>"><?php echo mapQuesType($feed['data'][$i]['type']) ?></td>
+                                        <td id="<?php echo 'type'.$i ?>"><?php echo mapQuesType($feed['data'][$i]['type']) ?></td>
                                         <td id="<?php echo 'subject'.$i ?>"><?php echo $feed['data'][$i]['subject'] ?></td>
                                         <td id="<?php echo 'chapter'.$i ?>"><?php echo $feed['data'][$i]['chapter'] ?></td>
                                         <td id="<?php echo 'topic'.$i ?>"><?php echo $feed['data'][$i]['topic'] ?></td>
                                         <td id="<?php echo 'answer'.$i ?>"><?php echo htmlspecialchars_decode($feed['data'][$i]['answer']) ?></td>
-                                        <td id="<?php echo 'level0'.$i ?>"><?php echo mapDifficulty($feed['data'][$i]['level']) ?></td>
+                                        <td id="<?php echo 'level'.$i ?>"><?php echo mapDifficulty($feed['data'][$i]['level']) ?></td>
                                         <td id="<?php echo 'marks'.$i ?>"><?php echo $feed['data'][$i]['marks'] ?></td>
                                         <!--td>
                                            <a target="_blank" href = "< ?php echo $feed['data'][$i]['ques_img'] ?>" class = "thumbnail">
@@ -164,9 +164,9 @@ else
                                         <td class="hide" id="<?php echo 'mcq2'.$i ?>"><?php echo $feed['data'][$i]['option2'] ?></td>
                                         <td class="hide" id="<?php echo 'mcq3'.$i ?>"><?php echo $feed['data'][$i]['option3'] ?></td>
                                         <td class="hide" id="<?php echo 'mcq4'.$i ?>"><?php echo $feed['data'][$i]['option4'] ?></td>
-                                        <td class="hide" id="<?php echo 'type'.$i ?>"><?php echo $feed['data'][$i]['type'] ?></td>
+                                        <td class="hide" id="<?php echo 'type0'.$i ?>"><?php echo $feed['data'][$i]['type'] ?></td>
                                         <td class="hide" id="<?php echo 'ques_img'.$i ?>"><?php echo $feed['data'][$i]['ques_img'] ?></td>
-                                        <td class="hide" id="<?php echo 'level'.$i ?>"><?php echo $feed['data'][$i]['level'] ?></td>
+                                        <td class="hide" id="<?php echo 'level0'.$i ?>"><?php echo $feed['data'][$i]['level'] ?></td>
                                         
 
 
--- role_admin/js/inlineeditquestion.js
+++ role_admin/js/inlineeditquestion.js
@@ -26,36 +26,45 @@ function saveQuestion(questionid, rowid){
     //var question=$('#question'+rowid+' textarea').val();
     //var answer=$('#answer'+rowid+' textarea').val();
     var columns=getColumnsArray();
+    var columnsMap=new Array();
     for(var i in columns){
-
+        var column=columns[i];
+        columnsMap[column]=$('#'+column+rowid+' textarea').val();
     }
-    var mcq1=$('#mcq1'+rowid).html();
-    var mcq2=$('#mcq2'+rowid).html();
-    var mcq3=$('#mcq3'+rowid).html();
-    var mcq4=$('#mcq4'+rowid).html();
-    var file=$('#ques_img'+rowid).html();
 
-    console.log(rowid+' '+file+' '+questionid+' '+topic+' '+cls+' '+type+' '+subject+' '+chapter+' '+tag+' '+level+' '+marks+' '+question+' '+answer+' '+mcq1+' '+mcq2+' '+mcq3+' '+mcq4);
+    columnsMap['mcq1']=$('#mcq1'+rowid).html();
+    columnsMap['mcq2']=$('#mcq2'+rowid).html();
+    columnsMap['mcq3']=$('#mcq3'+rowid).html();
+    columnsMap['mcq4']=$('#mcq4'+rowid).html();
+    columnsMap['file']=$('#ques_img'+rowid).html();
+
+    console.log('Columns map:');
+    console.log(columnsMap);
 
     var form = new FormData();
     form.append("func", "editquestion");
-    form.append("id", questionid);
-    form.append("question", question); 
-    form.append("answer", answer); 
-    form.append("mcq1", mcq1);
-    form.append("mcq2", mcq2);
-    form.append("mcq3", mcq3);
-    form.append("mcq4", mcq4); 
-    form.append("class", cls);
-    form.append("subject", subject);
-    form.append("type", type);
-    form.append("tag", tag);
-    form.append("chapter", chapter);
-    form.append("topic", topic);
-    form.append("level", level);
-    form.append("marks", marks);
-    form.append("link", link);
-    form.append("file", file);
+    
+    for(var key in columnsMap){
+        form.append(key,columnsMap[key]);
+    }
+
+    // form.append("id", questionid);
+    // form.append("question", question); 
+    // form.append("answer", answer); 
+    // form.append("mcq1", mcq1);
+    // form.append("mcq2", mcq2);
+    // form.append("mcq3", mcq3);
+    // form.append("mcq4", mcq4); 
+    // form.append("class", cls);
+    // form.append("subject", subject);
+    // form.append("type", type);
+    // form.append("tag", tag);
+    // form.append("chapter", chapter);
+    // form.append("topic", topic);
+    // form.append("level", level);
+    // form.append("marks", marks);
+    // form.append("link", link);
+    // form.append("file", file);
 
     var settings = {
     "async": true,
@@ -69,8 +78,14 @@ function saveQuestion(questionid, rowid){
     }
 
     $.ajax(settings).done(function (response) {
-        $('#youtube'+rowid).html(link);
-        $('#marks'+rowid).html(marks);
-        $('#tag'+rowid).html(tag);
+
+        for(var key in columnsMap){
+            var value=columnsMap[key];
+            $('#'+key+rowid).html(value);
+        }
+
+        // $('#youtube'+rowid).html(link);
+        // $('#marks'+rowid).html(marks);
+        // $('#tag'+rowid).html(tag);
     });
 }   
