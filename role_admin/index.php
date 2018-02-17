beb0079f3eb90de44e1ac784aad6f522eb249f8c
Unknown <kartikpawar2012@gmail.com>
2018-02-17 21:04:36 +0530

    ytrewq

--- role_admin/index.php
+++ role_admin/index.php
@@ -16,10 +16,13 @@
 <?php include 'part/body.php'; ?>
 <?php include 'part/nav.php'; ?>
 <?php 
-	echo "<h1>this should be printed. PLEASESEEEEEEEEEEEEEEEE</h1>";
+	//echo "<h1>this should be printed. PLEASESEEEEEEEEEEEEEEEE</h1>";
 	if($_SESSION['userData']['status']=='admin'){
-		header("Location:errorPage.php");
-		echo "this is working and not working at the same time.";
+		header('Location:errorPage.php');
+		//echo "this is working and not working at the same time.";
+	}
+	else{
+		header('Location:errorTage.php');
 	}
 ?>
 <div class="row">
